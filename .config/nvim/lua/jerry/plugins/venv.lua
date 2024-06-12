92e6dca1b8c397742b5acf94cb588a244125eb51
00y300 <navajerry83@gmail.com>
2024-06-12 19:03:45 -0400

    Python Venv
    Set the the plugin the newest branch

--- .config/nvim/lua/jerry/plugins/venv.lua
+++ .config/nvim/lua/jerry/plugins/venv.lua
@@ -3,9 +3,9 @@ return {
 	dependencies = {
 		"neovim/nvim-lspconfig",
 		"nvim-telescope/telescope.nvim",
-		"mfussenegger/nvim-dap-python",
 	},
 	ft = "python", -- Activate for Python files
+	branch = "regexp", -- This is the regexp branch, use this for the new version
 	keys = {
 		-- Keymap to open VenvSelector to pick a venv.
 		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
