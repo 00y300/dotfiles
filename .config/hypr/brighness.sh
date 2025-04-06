e2d76143a5683b45b80cb21a9490cf21fbb9d7cb
00y300 <navajerry83@gmail.com>
2025-04-06 16:28:21 -0400

    hyprland
    hyprland and brighness changes

--- .config/hypr/brighness.sh
+++ .config/hypr/brighness.sh
@@ -1,22 +1,36 @@
 #!/usr/bin/env sh
 
-# Accept an arg '+' or '-'
+# Accepts '+' or '-' as argument
 direction=$1
 
-# Get monitor info
+# Get monitor info from Hyprland
 monitor_data=$(hyprctl monitors -j)
-focused_name=$(echo $monitor_data | jq -r '.[] | select(.focused == true) | .name')
+focused_monitor=$(echo "$monitor_data" | jq -r '.[] | select(.focused == true) | .name')
 
-if [ "$focused_name" == "eDP-1" ]; then
-    # Internal display is focused -> use brillo
-    if [ "$direction" == "-" ]; then
+# Internal display
+if [ "$focused_monitor" = "eDP-1" ]; then
+    if [ "$direction" = "-" ]; then
         brillo -u 150000 -U 3
     else
         brillo -u 150000 -A 3
     fi
 else
-    # External display is focused -> use ddcutil
-    # But *which* external display?
-    focused_id=$(echo $monitor_data | jq -r '.[] | select(.focused == true) | .id')
-    ddcutil --display=$focused_id setvcp 10 $direction 8
+    # External monitor – map Hyprland names to ddcutil display numbers (or use --bus=)
+    case "$focused_monitor" in
+        "DP-1")
+            ddc_display=1  # Change this according to your `ddcutil detect` output
+            ;;
+        "DP-2")
+            ddc_display=2  # Change this according to your `ddcutil detect` output
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+
+    if [ "$direction" = "-" ]; then
+        ddcutil --display=$ddc_display setvcp 10 - 5
+    else
+        ddcutil --display=$ddc_display setvcp 10 + 5
+    fi
 fi
