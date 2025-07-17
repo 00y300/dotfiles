69d58d210a70123888ea127544b4b1710594cb6a
00y300 <navajerry83@gmail.com>
2025-07-16 20:34:36 -0400

    more changes
    more changes

--- .config/nvim/lua/jerry/plugins/alpha.lua
+++ .config/nvim/lua/jerry/plugins/alpha.lua
@@ -1,128 +1,129 @@
 return {
-	"goolord/alpha-nvim",
-	event = "VimEnter",
-	config = function()
-		-- Define custom highlight groups
-		vim.cmd([[
+  "goolord/alpha-nvim",
+  enabled = false,
+  event = "VimEnter",
+  config = function()
+    -- Define custom highlight groups
+    vim.cmd([[
       highlight AlphaNeovimLogoBlue guifg=#5fafd7
       highlight AlphaNeovimLogoGreen guifg=#87af5f
       highlight AlphaNeovimLogoGreenFBlueB guifg=#87af5f guibg=#5fafd7
     ]])
 
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
 
-		-- Set new header
-		local logo = {
-			[[ ███       ███ ]],
-			[[████      ████]],
-			[[██████     █████]],
-			[[███████    █████]],
-			[[████████   █████]],
-			[[█████████  █████]],
-			[[█████ ████ █████]],
-			[[█████  █████████]],
-			[[█████   ████████]],
-			[[█████    ███████]],
-			[[█████     ██████]],
-			[[████      ████]],
-			[[ ███       ███ ]],
-			[[                  ]],
-			[[ N  E  O  V  I  M ]],
-		}
-		dashboard.section.header.val = logo
-		dashboard.section.header.opts.hl = {
-			{
-				{ "AlphaNeovimLogoBlue", 0, 0 },
-				{ "AlphaNeovimLogoGreen", 1, 14 },
-				{ "AlphaNeovimLogoBlue", 23, 34 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 2 },
-				{ "AlphaNeovimLogoGreenFBlueB", 2, 4 },
-				{ "AlphaNeovimLogoGreen", 4, 19 },
-				{ "AlphaNeovimLogoBlue", 27, 40 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 4 },
-				{ "AlphaNeovimLogoGreenFBlueB", 4, 7 },
-				{ "AlphaNeovimLogoGreen", 7, 22 },
-				{ "AlphaNeovimLogoBlue", 29, 42 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 8 },
-				{ "AlphaNeovimLogoGreenFBlueB", 8, 10 },
-				{ "AlphaNeovimLogoGreen", 10, 25 },
-				{ "AlphaNeovimLogoBlue", 31, 44 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 10 },
-				{ "AlphaNeovimLogoGreenFBlueB", 10, 13 },
-				{ "AlphaNeovimLogoGreen", 13, 28 },
-				{ "AlphaNeovimLogoBlue", 33, 46 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 13 },
-				{ "AlphaNeovimLogoGreen", 14, 31 },
-				{ "AlphaNeovimLogoBlue", 35, 49 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 13 },
-				{ "AlphaNeovimLogoGreen", 16, 32 },
-				{ "AlphaNeovimLogoBlue", 35, 49 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 13 },
-				{ "AlphaNeovimLogoGreen", 17, 33 },
-				{ "AlphaNeovimLogoBlue", 35, 49 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 13 },
-				{ "AlphaNeovimLogoGreen", 18, 34 },
-				{ "AlphaNeovimLogoGreenFBlueB", 33, 35 },
-				{ "AlphaNeovimLogoBlue", 35, 49 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 13 },
-				{ "AlphaNeovimLogoGreen", 19, 35 },
-				{ "AlphaNeovimLogoGreenFBlueB", 34, 35 },
-				{ "AlphaNeovimLogoBlue", 35, 49 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 13 },
-				{ "AlphaNeovimLogoGreen", 20, 36 },
-				{ "AlphaNeovimLogoGreenFBlueB", 35, 37 },
-				{ "AlphaNeovimLogoBlue", 37, 49 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 0, 13 },
-				{ "AlphaNeovimLogoGreen", 21, 37 },
-				{ "AlphaNeovimLogoGreenFBlueB", 36, 37 },
-				{ "AlphaNeovimLogoBlue", 37, 49 },
-			},
-			{
-				{ "AlphaNeovimLogoBlue", 1, 13 },
-				{ "AlphaNeovimLogoGreen", 20, 35 },
-				{ "AlphaNeovimLogoBlue", 37, 48 },
-			},
-			{},
-			{ { "AlphaNeovimLogoGreen", 0, 9 }, { "AlphaNeovimLogoBlue", 9, 18 } },
-		}
+    -- Set new header
+    local logo = {
+      [[ ███       ███ ]],
+      [[████      ████]],
+      [[██████     █████]],
+      [[███████    █████]],
+      [[████████   █████]],
+      [[█████████  █████]],
+      [[█████ ████ █████]],
+      [[█████  █████████]],
+      [[█████   ████████]],
+      [[█████    ███████]],
+      [[█████     ██████]],
+      [[████      ████]],
+      [[ ███       ███ ]],
+      [[                  ]],
+      [[ N  E  O  V  I  M ]],
+    }
+    dashboard.section.header.val = logo
+    dashboard.section.header.opts.hl = {
+      {
+        { "AlphaNeovimLogoBlue", 0, 0 },
+        { "AlphaNeovimLogoGreen", 1, 14 },
+        { "AlphaNeovimLogoBlue", 23, 34 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 2 },
+        { "AlphaNeovimLogoGreenFBlueB", 2, 4 },
+        { "AlphaNeovimLogoGreen", 4, 19 },
+        { "AlphaNeovimLogoBlue", 27, 40 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 4 },
+        { "AlphaNeovimLogoGreenFBlueB", 4, 7 },
+        { "AlphaNeovimLogoGreen", 7, 22 },
+        { "AlphaNeovimLogoBlue", 29, 42 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 8 },
+        { "AlphaNeovimLogoGreenFBlueB", 8, 10 },
+        { "AlphaNeovimLogoGreen", 10, 25 },
+        { "AlphaNeovimLogoBlue", 31, 44 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 10 },
+        { "AlphaNeovimLogoGreenFBlueB", 10, 13 },
+        { "AlphaNeovimLogoGreen", 13, 28 },
+        { "AlphaNeovimLogoBlue", 33, 46 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 13 },
+        { "AlphaNeovimLogoGreen", 14, 31 },
+        { "AlphaNeovimLogoBlue", 35, 49 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 13 },
+        { "AlphaNeovimLogoGreen", 16, 32 },
+        { "AlphaNeovimLogoBlue", 35, 49 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 13 },
+        { "AlphaNeovimLogoGreen", 17, 33 },
+        { "AlphaNeovimLogoBlue", 35, 49 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 13 },
+        { "AlphaNeovimLogoGreen", 18, 34 },
+        { "AlphaNeovimLogoGreenFBlueB", 33, 35 },
+        { "AlphaNeovimLogoBlue", 35, 49 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 13 },
+        { "AlphaNeovimLogoGreen", 19, 35 },
+        { "AlphaNeovimLogoGreenFBlueB", 34, 35 },
+        { "AlphaNeovimLogoBlue", 35, 49 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 13 },
+        { "AlphaNeovimLogoGreen", 20, 36 },
+        { "AlphaNeovimLogoGreenFBlueB", 35, 37 },
+        { "AlphaNeovimLogoBlue", 37, 49 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 0, 13 },
+        { "AlphaNeovimLogoGreen", 21, 37 },
+        { "AlphaNeovimLogoGreenFBlueB", 36, 37 },
+        { "AlphaNeovimLogoBlue", 37, 49 },
+      },
+      {
+        { "AlphaNeovimLogoBlue", 1, 13 },
+        { "AlphaNeovimLogoGreen", 20, 35 },
+        { "AlphaNeovimLogoBlue", 37, 48 },
+      },
+      {},
+      { { "AlphaNeovimLogoGreen", 0, 9 }, { "AlphaNeovimLogoBlue", 9, 18 } },
+    }
 
-		-- Set menu
-		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
-		}
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+    }
 
-		-- Send config to alpha
-		alpha.setup(dashboard.opts)
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
 
-		-- Disable folding on alpha buffer
-		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-	end,
+    -- Disable folding on alpha buffer
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
 }
--- .config/nvim/lua/jerry/plugins/lsp/debug.lua
+++ .config/nvim/lua/jerry/plugins/lsp/debug.lua
@@ -10,22 +10,34 @@ return {
         "microsoft/vscode-js-debug",
         version = "1.*",
         build = "npm i && npm run compile vsDebugServerBundle && mv dist out",
+        lazy = true,
       },
       {
         "theHamsta/nvim-dap-virtual-text",
-
+        lazy = true,
+        opts = {
+          enabled = true,
+          enabled_commands = true,
+          highlight_changed_variables = true,
+          highlight_new_as_changed = false,
+          show_stop_reason = true,
+          commented = false,
+          only_first_definition = true,
+          all_references = false,
+          clear_on_continue = false,
+        },
+      },
+      {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        lazy = true,
         config = function()
-          require("nvim-dap-virtual-text").setup({
-            enabled = true, -- enable this plugin (the default)
-            enabled_commands = true, -- create commands for toggling and refreshing
-            highlight_changed_variables = true,
-            highlight_new_as_changed = false,
-            show_stop_reason = true, -- show stop reason when exceptions occur
-            commented = false, -- do not prefix virtual text with comment string
-            only_first_definition = true, -- show text only at the first variable definition
-            all_references = false,
-            clear_on_continue = false, -- avoid flickering on "continue"
-          })
+          local python_path = vim.fn.trim(vim.fn.system("which python"))
+          if vim.fn.executable(python_path) == 0 then
+            vim.notify("Python with debugpy not found: " .. python_path, vim.log.levels.ERROR)
+            return
+          end
+          require("dap-python").setup(python_path)
         end,
       },
     },
@@ -40,11 +52,25 @@ return {
       { "<leader>dr", "<cmd>DapReplToggle<CR>", desc = "Toggle REPL" },
       { "<leader>de", "<cmd>lua require('dapui').eval()<CR>", desc = "Evaluate Expression" },
     },
+    cmd = {
+      "DapContinue",
+      "DapToggleBreakpoint",
+      "DapStepOver",
+      "DapStepInto",
+      "DapStepOut",
+      "DapClearBreakpoints",
+      "DapTerminate",
+      "DapReplToggle",
+    },
     config = function()
       local dap = require("dap")
-      local cwd = vim.fn.getcwd()
 
-      -- Define signs for various debugger states.
+      -- Cache expensive operations
+      local cwd = vim.fn.getcwd()
+      local data_path = vim.fn.stdpath("data")
+
+      -- Setup signs efficiently
+
       local icons = {
         Stopped = { "", "DiagnosticWarn", "DapStoppedLine" },
         Breakpoint = { "", "DiagnosticInfo" },
@@ -52,6 +78,7 @@ return {
         BreakpointCondition = { "", "DiagnosticInfo" },
         LogPoint = { ".>", "DiagnosticInfo" },
       }
+
       for name, sign in pairs(icons) do
         vim.fn.sign_define("Dap" .. name, {
           text = sign[1],
@@ -61,128 +88,183 @@ return {
         })
       end
 
-      -- Setup JavaScript/TypeScript debugging via vscode-js-debug using your file path.
-      require("dap-vscode-js").setup({
-        debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
-        adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost", "firefox" },
-      })
+      -- Lazy setup for language-specific configurations
+      local function setup_js_debug()
+        require("dap-vscode-js").setup({
+          debugger_path = data_path .. "/lazy/vscode-js-debug",
+          adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost", "firefox" },
+        })
+      end
 
-      -- Setup Firefox adapter using your nix-based path.
-      local firefoxAdapterPath = vim.fn.trim(
-        vim.fn.system("nix eval --raw nixpkgs#vscode-extensions.firefox-devtools.vscode-firefox-debug.outPath")
-      ) .. "/share/vscode/extensions/firefox-devtools.vscode-firefox-debug/dist/adapter.bundle.js"
-      dap.adapters.firefox = {
-        type = "executable",
-        command = "node",
-        args = { firefoxAdapterPath },
-      }
+      local function setup_firefox_adapter()
+        local ok, firefox_path = pcall(
+          vim.fn.trim,
+          vim.fn.system(
+            "nix eval --raw nixpkgs#vscode-extensions.firefox-devtools.vscode-firefox-debug.outPath 2>/dev/null"
+          )
+        )
 
-      -- Helper function to prompt for a URL using a coroutine.
+        if ok and firefox_path ~= "" then
+          dap.adapters.firefox = {
+            type = "executable",
+            command = "node",
+            args = {
+              firefox_path .. "/share/vscode/extensions/firefox-devtools.vscode-firefox-debug/dist/adapter.bundle.js",
+            },
+          }
+        end
+      end
+
       local function prompt_url(default)
-        local co = coroutine.running()
-        return coroutine.create(function()
+        return coroutine.wrap(function()
+          local co = coroutine.running()
           vim.ui.input({ prompt = "Enter URL: ", default = default }, function(url)
-            if url and url ~= "" then
-              coroutine.resume(co, url)
-            end
+            coroutine.resume(co, url or default)
           end)
+          return coroutine.yield()
         end)
       end
 
-      -- Configure debugging for JavaScript-based languages.
-      local js_configs = {
-        {
-          type = "firefox",
-          name = "Launch Firefox to debug 󰈹",
-          request = "launch",
-          reAttach = true,
-          url = function()
-            return prompt_url("http://localhost:3000")
-          end,
-          webRoot = cwd,
-          firefoxExecutable = vim.fn.trim(vim.fn.system("readlink -e $(which firefox)")),
-        },
-        {
-          type = "pwa-chrome",
-          request = "launch",
-          name = "Launch & Debug Chrome ",
-          url = function()
-            return prompt_url("http://localhost:3000")
-          end,
-          webRoot = cwd,
-          protocol = "inspector",
-          sourceMaps = true,
-          userDataDir = false,
-        },
-      }
-      for _, lang in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" }) do
-        dap.configurations[lang] = js_configs
+      -- Language-specific configuration functions
+      local function get_js_configs()
+        local configs = {
+          {
+            type = "pwa-chrome",
+            request = "launch",
+            name = "Launch & Debug Chrome ",
+            url = function()
+              return prompt_url("http://localhost:3000")
+            end,
+            webRoot = cwd,
+            protocol = "inspector",
+            sourceMaps = true,
+            userDataDir = false,
+          },
+        }
+
+        -- Only add Firefox config if adapter is available
+        if dap.adapters.firefox then
+          table.insert(configs, 1, {
+            type = "firefox",
+            name = "Launch Firefox to debug 󰈹",
+            request = "launch",
+            reAttach = true,
+            url = function()
+              return prompt_url("http://localhost:3000")
+            end,
+            webRoot = cwd,
+            firefoxExecutable = vim.fn.trim(vim.fn.system("readlink -e $(which firefox) 2>/dev/null || echo ''")),
+          })
+        end
+
+        return configs
       end
 
-      dap.configurations.go = {
-        {
-          type = "go",
-          name = "Debug current file",
-          request = "launch",
-          program = "${file}", -- Tells Delve to debug whatever file is currently open
-        },
-        {
-          type = "go",
-          name = "Debug current file (with args)",
-          request = "launch",
-          program = "${file}",
-          args = require("dap-go").get_arguments, -- Prompts you for command-line args
-        },
-      }
-      -- Setup GDB adapter and configurations for C, C++, and Rust.
-      dap.adapters.gdb = {
-        type = "executable",
-        command = vim.fn.trim(vim.fn.system("readlink -e $(which gdb)")),
-        args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
-      }
-      local gdb_config = {
-        {
-          name = "Launch",
-          type = "gdb",
-          request = "launch",
-          program = function()
-            return vim.fn.input("Path to executable: ", cwd .. "/", "file")
-          end,
-          cwd = "${workspaceFolder}",
-          stopOnEntry = false,
-          stopAtBeginningOfMainSubprogram = false,
-        },
-        {
-          name = "Select and attach to process",
-          type = "gdb",
-          request = "attach",
-          program = function()
-            return vim.fn.input("Path to executable: ", cwd .. "/", "file")
-          end,
-          pid = function()
-            local filter = vim.fn.input("Executable name (filter): ")
-            return require("dap.utils").pick_process({ filter = filter })
-          end,
-          cwd = "${workspaceFolder}",
-        },
-        {
-          name = "Attach to gdbserver :1234",
-          type = "gdb",
-          request = "attach",
-          target = "localhost:1234",
-          program = function()
-            return vim.fn.input("Path to executable: ", cwd .. "/", "file")
-          end,
-          cwd = "${workspaceFolder}",
-        },
-      }
-      dap.configurations.c = gdb_config
-      dap.configurations.cpp = gdb_config
-      dap.configurations.rust = gdb_config
+      local function get_go_configs()
+        return {
+          {
+            type = "go",
+            name = "Debug current file",
+            request = "launch",
+            program = "${file}",
+          },
+          {
+            type = "go",
+            name = "Debug current file (with args)",
+            request = "launch",
+            program = "${file}",
+            args = require("dap-go").get_arguments,
+          },
+        }
+      end
 
-      -- Initialize DAP UI and dap-go.
+      local function get_gdb_configs()
+        return {
+          {
+            name = "Launch",
+            type = "gdb",
+            request = "launch",
+            program = function()
+              return vim.fn.input("Path to executable: ", cwd .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+            stopOnEntry = false,
+            stopAtBeginningOfMainSubprogram = false,
+          },
+          {
+            name = "Select and attach to process",
+            type = "gdb",
+            request = "attach",
+            program = function()
+              return vim.fn.input("Path to executable: ", cwd .. "/", "file")
+            end,
+            pid = function()
+              local filter = vim.fn.input("Executable name (filter): ")
+              return require("dap.utils").pick_process({ filter = filter })
+            end,
+            cwd = "${workspaceFolder}",
+          },
+          {
+            name = "Attach to gdbserver :1234",
+            type = "gdb",
+            request = "attach",
+            target = "localhost:1234",
+            program = function()
+              return vim.fn.input("Path to executable: ", cwd .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+
+      -- Lazy configuration setup based on filetype
+      local function setup_language_configs()
+        local ft = vim.bo.filetype
+
+        if vim.tbl_contains({ "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" }, ft) then
+          if not dap.configurations[ft] then
+            setup_js_debug()
+            setup_firefox_adapter()
+            local js_configs = get_js_configs()
+            for _, lang in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" }) do
+              dap.configurations[lang] = js_configs
+            end
+          end
+        elseif ft == "go" then
+          if not dap.configurations.go then
+            dap.configurations.go = get_go_configs()
+          end
+        elseif vim.tbl_contains({ "c", "cpp", "rust" }, ft) then
+          if not dap.configurations[ft] then
+            -- Setup GDB adapter lazily
+            if not dap.adapters.gdb then
+              dap.adapters.gdb = {
+                type = "executable",
+                command = vim.fn.trim(vim.fn.system("readlink -e $(which gdb) 2>/dev/null || echo 'gdb'")),
+                args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+              }
+            end
+            local gdb_configs = get_gdb_configs()
+            dap.configurations.c = gdb_configs
+            dap.configurations.cpp = gdb_configs
+            dap.configurations.rust = gdb_configs
+          end
+        end
+      end
+
+      -- Setup language configs on first debug command
+      local original_continue = dap.continue
+      dap.continue = function()
+        setup_language_configs()
+        dap.continue = original_continue -- Restore original function
+        return dap.continue()
+      end
+
+      -- Setup UI and Go plugin
       require("dapui").setup()
       require("dap-go").setup()
+
+      -- UI event handlers
       local dapui = require("dapui")
       dap.listeners.after.event_initialized["dapui_config"] = dapui.open
       dap.listeners.before.event_terminated["dapui_config"] = dapui.close
