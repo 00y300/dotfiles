cdc20d3a016a9eba672ec751d3b95474cf4a3c10
00y300 <navajerry83@gmail.com>
2025-09-19 21:01:12 -0400

    changes to rust
    changes to rust

--- .config/nvim/lua/jerry/plugins/java.lua
+++ .config/nvim/lua/jerry/plugins/java.lua
@@ -37,31 +37,5 @@ return {
         invalid_mason_registry = false,
       },
     })
-    require("lspconfig").rust_analyzer.setup({
-      handlers = {
-        -- By assigning an empty function, you can remove the notifications
-        -- printed to the cmd
-        ["$/progress"] = function(_, result, ctx) end,
-      },
-    })
-    require("lspconfig").jdtls.setup({
-      handlers = {
-        ["$/progress"] = function(_, result, ctx) end,
-      },
-      -- settings = {
-      --   java = {
-      --     configuration = {
-      --       runtimes = {
-      --         {
-      --           name = "OpenJDK21",
-      --           -- path = "~/.asdf/installs/java/openjdk-21",
-      --           path = "~/.asdf/installs/java/openjdk-21/",
-      --           default = true,
-      --         },
-      --       },
-      --     },
-      --   },
-      -- },
-    })
   end,
 }
