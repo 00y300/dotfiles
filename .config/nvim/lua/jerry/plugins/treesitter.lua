2f10b28e266fc1b77a009006ee677b66df0e84cf
00y300 <72098565+00y300@users.noreply.github.com>
2026-01-27 21:44:00 -0500

    c
    c

--- .config/nvim/lua/jerry/plugins/treesitter.lua
+++ .config/nvim/lua/jerry/plugins/treesitter.lua
@@ -2,10 +2,8 @@ return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-
-    local parsers = {
+  opts = {
+    ensure_installed = {
       "json",
       "javascript",
       "typescript",
@@ -33,29 +31,20 @@ return {
       "go",
       "elixir",
       "heex",
-    }
+    },
 
-    configs.setup({
-      ensure_installed = parsers,
-      auto_install = true,
+    auto_install = true,
 
-      highlight = {
-        enable = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        node_decremental = "<bs>",
       },
-
-      indent = {
-        enable = true,
-      },
-
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
+    },
+  },
 }
